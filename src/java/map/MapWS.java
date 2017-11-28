b270f393c4029f1dd6682afb549d665c28a99a1a
Jacek Polański <kontakt@jpolanski.pl>
2017-11-28 01:15:33 +0100

    [BUGFIX] changed path to map

--- src/java/map/MapWS.java
+++ src/java/map/MapWS.java
@@ -8,12 +8,14 @@ import javax.jws.WebMethod;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  *
  * @author jacek
  */
-@WebService(serviceName = "MapWS")
+@WebService(serviceName = "MapWS", targetNamespace = "map")
 @Stateless()
 public class MapWS {
 
@@ -23,18 +25,17 @@ public class MapWS {
      * @throws java.lang.Exception 
      */
     @WebMethod(operationName = "getMap")
-    public byte[] getMap() throws Exception {
-        String sourceFile = "/home/jacek/Obrazy/pultusk.png";
+    public byte[] getMap() throws Exception {        
+        URL sourceFile = this.getClass().getClassLoader().getResource("pultusk.png");
         
         Base64.Encoder encoder = Base64.getEncoder();
-        byte[] mapData = loadFileAsBytesArray(sourceFile);
+        byte[] mapData = loadFileAsBytesArray(sourceFile.getPath());
         byte[] encodedMap = encoder.encode(mapData);
         
         return encodedMap;
     }
     
     public static byte[] loadFileAsBytesArray(String fileName) throws Exception {
-
         File file = new File(fileName);
         int length = (int) file.length();
         BufferedInputStream reader;
@@ -44,6 +45,5 @@ public class MapWS {
         reader.close();
         
         return bytes;
-
     }
 }
